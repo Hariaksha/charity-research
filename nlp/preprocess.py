782ae18f4e1ac5ed1f9cd6a99cd8db29e1228e36
Hariaksha <hariakshagunda@gmail.com>
2024-03-15 23:44:53 -0500

    Update preprocess.py

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -2,25 +2,31 @@ import spacy
 from spacy.tokens import doc
 import openpyxl
 
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_md')
+print("Loaded model")
 
-workbook = openpyxl.load_workbook(f'data/preliminary_march_data/data.xlsx')
+workbook = openpyxl.load_workbook('data/preliminary_march_data/data.xlsx')
 ws = workbook.active
+print("opened workbook 1")
 
 workbook2 = openpyxl.load_workbook('nlp/preprocessed-missions.xlsx')
 ws2 = workbook2.active
+print("opened workbook 2")
 
 count = 0
 for i in range(2, ws.max_row + 1):
-    if count == 100:
+    print(i)
+    if count == 3000:
         break
-    mission = ws[f'P{i}']
-    ein = ws[f'A{i}']
-    name = ws[f'B{i}']
-    ntee = ws['I{i}']
-    asset = ws['K{i}']
-    revenue = ws['O{i}']
+    mission = str(ws[f'P{i}'].value)
+    ein = ws[f'A{i}'].value
+    name = ws[f'B{i}'].value
+    ntee = ws[f'I{i}'].value
+    asset = ws[f'K{i}'].value
+    revenue = ws[f'O{i}'].value
     doc = nlp(mission)
-    ws2.append([ein, name, mission, doc, revenue, asset, ntee])
+    lemmatized_text = " ".join([token.lemma_ for token in doc])
+    ws2.append([ein, name, "", lemmatized_text, revenue, asset, ntee])
+    count += 1
 
-workbook2.save()
+workbook2.save('nlp/preprocessed-missions.xlsx')
