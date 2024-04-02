d36b48c21a3cd79721c6f29d1c58b22d1a872000
Hariaksha <hariakshagunda@gmail.com>
2024-04-01 20:11:09 -0500

    Delete nlp.py

--- nlp/nlp.py
+++
@@ -1,4 +0,0 @@
-import spacy # free open-source library for NLP in Python. features NER, POS tagging, dependency parsing, word vectors, and more
-import numpy as np
-from spacy.matcher import Matcher
-
