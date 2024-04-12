3e9211c2add6a917237c95da8b3362e7f53effe3
Hariaksha <hariakshagunda@gmail.com>
2024-04-12 13:50:05 -0500

    Update preprocess.py

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -9,8 +9,6 @@ def main():
     print("Opened workbook")
 
     for i in range(2, ws.max_row + 1):
-        if i % 100 == 0:
-            break
         mission = str(ws[f'P{i}'].value)
         doc = nlp(mission) # turn mission into a spaCy document
         preprocessed_text = " ".join([token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct]) # take lemmas of words while lowercasing text and removing punctuation and stop words
