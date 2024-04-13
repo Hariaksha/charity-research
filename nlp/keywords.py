ae50c2f3b4563c76f1e3171f6222659ea08f4b5a
Hariaksha <hariakshagunda@gmail.com>
2024-04-13 17:19:51 -0500

    Update keywords.py

--- nlp/keywords.py
+++ nlp/keywords.py
@@ -1,18 +1,23 @@
-import openpyxl, csv
+import openpyxl
 from collections import defaultdict
 
 def main():
     counts = defaultdict(int)
-    workbook = openpyxl.load_workbook(f'data/american/data.xlsx')
+    workbook = openpyxl.load_workbook(f'data/american/data.xlsx', read_only=True)
     ws = workbook.active
     for i in range(2, ws.max_row + 1):
-        words = ws[f'Q{i}'].value.split()
-        for word in words:
-            counts[word] += 1
-    filename = open('data/keywords.csv') 
-    file = csv.DictReader(filename)
+        if i % 1000 == 0:
+            print(i)
+        if ws[f'Q{i}'].value != None:
+            for word in ws[f'Q{i}'].value.split():
+                counts[word] += 1
+    workbook = openpyxl.load_workbook(f'data/american/data.xlsx', read_only=False)
+    ws = workbook.active
+    print("Number of words: ", len(counts))
     for word, count in counts.items():
-        file.append([word, count])
+        ws.append([word, count])
+    workbook.save('data/american/data.xlsx')
+    print("Saved")
 
 if __name__=="__main__":
     main()
