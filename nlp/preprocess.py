9c777d440026605bd6c98431339729d0e512006d
Hariaksha <hariakshagunda@gmail.com>
2024-05-12 13:50:20 -0500

    Update preprocess.py

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -1,6 +1,7 @@
-import spacy, openpyxl
+import spacy, openpyxl, datetime
 
 def main():
+    start = last = datetime.datetime.now()
     nlp = spacy.load('en_core_web_lg')
     print("Loaded model")
 
@@ -13,9 +14,12 @@ def main():
         doc = nlp(mission) # turn mission into a spaCy document
         preprocessed_text = " ".join([token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct]) # take lemmas of words while lowercasing text and removing punctuation and stop words
         ws[f'Q{i}'].value = str(preprocessed_text)
-
+        if i % 10000 == 0:
+            print("Finished", i)
+    last = datetime.datetime.now()
     workbook.save('data/american/data.xlsx')
-    print("saved")
+    print("Saved")
+    print("Start:", start.strftime("%d %b, %I:%M%p"), "\nLast:", last.strftime("%d %b, %I:%M%p"))
 
 if __name__=="__main__":
     main()
