4400c82740b749c69e1a2f7684c9b73595ab617b
Hariaksha <hariakshagunda@gmail.com>
2024-05-13 17:15:01 -0500

    Update keywords.py

--- nlp/keywords.py
+++ nlp/keywords.py
@@ -6,17 +6,17 @@ def main():
     workbook = openpyxl.load_workbook(f'data/american/data.xlsx', read_only=True)
     ws = workbook.active
     for i in range(2, ws.max_row + 1):
-        if i % 1000 == 0:
+        if i % 10000 == 0:
             print(i)
         if ws[f'Q{i}'].value != None:
             for word in ws[f'Q{i}'].value.split():
                 counts[word] += 1
-    workbook = openpyxl.load_workbook(f'data/american/data.xlsx', read_only=False)
+    workbook = openpyxl.load_workbook(f'data/american/keywords.xlsx', read_only=False)
     ws = workbook.active
     print("Number of words: ", len(counts))
     for word, count in counts.items():
         ws.append([word, count])
-    workbook.save('data/american/data.xlsx')
+    workbook.save('data/american/keywords.xlsx')
     print("Saved")
 
 if __name__=="__main__":
